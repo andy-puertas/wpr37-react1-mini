6fcae82bb94288bf559b446207d27d12e9ceed97
Andy Puertas <andy.puertas08@gmail.com>
2018-05-11 11:02:52 -0600

    initial upload pt. 2 lol

---
+++ react1-mini/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <p>HI my name is fred</p>
+          <p>Hellow</p>
+      </div>
+    );
+  }
+}
+
+export default App;
