16414db958e8f24ec18f9e5a2131652ff20dcbdf
Andy Puertas <andy.puertas08@gmail.com>
2018-05-11 13:09:21 -0600

    added header component

--- react1-mini/src/App.js
+++ react1-mini/src/App.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react';
+//import Header from './header.js'
+
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      sports: ['Baseball', 'Soccer', 'Football', 'Basketball'],
+      myName: 'Joe'
+    }
+  }
+
+  inputChange(val) {
+    this.setState({
+      myName: val
+    })
+  }
+
+  handleClick(sport){
+    alert(sport)
+  }
+
   render() {
+    let sportsJSX = this.state.sports.map( (sport,i) => {
+      return (
+        <div key={i}>
+          <h3>{sport}</h3>
+          <button onClick={() => this.handleClick(sport) }>Click to learn more about {sport}</button>  
+        </div>  
+      )
+    })
     return (
       <div>
-          <p>HI my name is fred</p>
-          <p>Hellow</p>
+        {/* <Header /> */}
+          <h1>{this.state.myName}</h1> 
+          <input value={this.state.myName} onChange={(e) => this.inputChange(e.target.value) }/>    
+          {sportsJSX}
       </div>
     );
   }
